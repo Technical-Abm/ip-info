a35ea7de2a0d2eccfd01043330110091dd5bdc57
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-08-27 02:36:28 -0700

    Update ipinfo.py

--- ipinfo.py
+++ ipinfo.py
@@ -91,9 +91,9 @@ if __name__ == '__main__':
                 ip_info()
             elif sys.version_info<(3,9):
                 os.system("clear")
-                  sys.stderr.write(" your are too old python version!")
-                  sys.stderr.write(" Please upgrade lastest python version")
-                  sys.stderr.write(" contact admin : https://www.facebook.com/techabm")
+                sys.stderr.write(" your are too old python version!")
+                sys.stderr.write(" Please upgrade lastest python version")
+                sys.stderr.write(" contact admin : https://www.facebook.com/techabm")
                 pass
             else:
                 print()
@@ -103,3 +103,4 @@ if __name__ == '__main__':
                 pass
             pass
     version()
+   #feel me more :)
